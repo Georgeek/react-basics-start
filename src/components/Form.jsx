149e14493edf5e7fce5e840691602b727156666d
Georgeek <egormad@gmail.com>
2017-01-23 17:14:51 +0800

    added form

---
+++ src/components/Form.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Button from './Button';
+
+class Form extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		console.log(event);
+	}
+
+	render() {
+		return (
+			<form className="todo-form" onSubmit={this.handleSubmit}>
+				<input type="text" placeholder="Что нужно сделать?" />
+
+				<Button type="submit"> Добавить </Button>
+			</form>
+		);
+	}
+}
+
+Form.propTypes = {
+
+};
+
+export default Form;
