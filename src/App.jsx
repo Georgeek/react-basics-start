65364e564e2ac9df6a79baa71c453b61ae99a5b6
Georgeek <egormad@gmail.com>
2017-01-30 17:22:24 +0800

    added ReactCSSTransitionGroup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import todos from './todos';
 import Header from './components/Header';
@@ -60,7 +61,7 @@ class App extends React.Component {
 			if (todo.id === id) {
 				todo.title = title;
 			}
-			
+
 			return todo;
 		});
 
@@ -72,7 +73,12 @@ class App extends React.Component {
 			<main>
 				<Header title={this.props.title} todos={this.state.todos} />
 
-				<section className="todo-list">
+				<ReactCSSTransitionGroup
+					component="section"
+					className="todo-list"
+					transitionName="slide"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={500}>
 					{this.state.todos.map(todo => // с помощью map мы перебираем массив todos (напоминание себе)
 						<Todo 
 							key={todo.id}
@@ -84,7 +90,7 @@ class App extends React.Component {
 							onEdit={this.handleEdit}
 						/>)
 					}
-				</section>
+				</ReactCSSTransitionGroup>
 
 				<Form onAdd={this.handleAdd} />
 			</main>
